80b5b77eef07bea31b4fe2dedc7787015a488fcf
Shahid-Ansari-06 <sa6888568@gmail.com>
2024-09-04 18:23:28 +0530

    Update contact me.js

--- contact me.js
+++ contact me.js
@@ -4,7 +4,7 @@ function goBack() {
 
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();  
-    alert('Thank you for your submission! Your form has been successfully sent.');  
+    alert('Thank you for your submission! Your form has been successfully sent. I will contact you on your email.');  
     
    
     this.reset();
